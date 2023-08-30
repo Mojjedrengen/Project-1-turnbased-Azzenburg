32c4259d060e48d54c48334b78ff0ae288d7059d
Mojjedrengen <99266094+Mojjedrengen@users.noreply.github.com>
2023-08-30 11:37:17 +0200

    The entire cast of Azzenburg is here

--- Assets/Scripts/character.cs
+++ Assets/Scripts/character.cs
@@ -5,7 +5,7 @@ public class character
 {
     public int hp;
     public int mp;
-    public enum Role {Fire, Arcane, Necromancer};
+    public enum Role {Fire, Arcane, Necromancer, Saber, Alchemist, Tera};
     public Role role;
     public String bio;
 }
